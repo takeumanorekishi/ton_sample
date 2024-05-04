8b1a009d9826c6156e668875a0e35a2b001a3d76
takeumanorekishi <takeumanorekishi@gmail.com>
2024-05-05 01:46:42 +0900

    fix config

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 // this manifest is used temporarily for development purposes
 const manifestUrl =
-  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+  "https://takeumanorekishi.github.io/ton_sample/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
